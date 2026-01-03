c45bf1d27c487905f9afe726869848932a45d83f
Seven_Sec <1260493648@qq.com>
2026-01-03 15:55:55 +0800

    feat(home-page): drop some animation and add more customizing supports

--- src/custom/site-config.ts
+++ src/custom/site-config.ts
@@ -5,9 +5,23 @@ const siteConfig = {
   authorGreet: '<your-author-greet>',
   authorDescription: '<your-author-description>',
   socialLinks: [
-    { name: 'GitHub', url: 'https://github.com/SevenSec114' },
+    { name: 'GitHub', url: 'https://github.com' },  // Replace with your GitHub URL
     // You can add more social links here
   ],
+  /**
+   * Header background configuration
+   * This will determine the home page's header background, using a default gradient or a media file.
+   */
+  headerBackground: {
+    /** Available: 'default' (default gradient background), 'custom' (image/video) */
+    type: 'default',
+
+    /** The path to the midea file, they must be in the public folder. If the relative path is `public/img.png`, it will be `/img.png`.
+     *  Only available if headerBackground.type is `custom`.
+     *  Also note that please make sure the media resolution is appropriate for the background's size, or it will be cropped.
+     */
+    mediaPath: ''
+  },
 };
 
 export default siteConfig;
